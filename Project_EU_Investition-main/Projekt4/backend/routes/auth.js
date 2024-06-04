deac14272c4652217e38d626cb3d06361f0d57a6
m2508-o <92919445+m2508-o@users.noreply.github.com>
2024-06-04 13:18:07 +0200

    Update auth.js

--- Project_EU_Investition-main/Projekt4/backend/routes/auth.js
+++ Project_EU_Investition-main/Projekt4/backend/routes/auth.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs');
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
   try {
-    // Hash the password
+    
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({ username, password: hashedPassword });
@@ -27,12 +27,6 @@ router.post('/login', async (req, res) => {
       return res.status(401).send('User not found');
     }
     const validPassword = await bcrypt.compare(password, user.password);
-    //const hashedPassword = await bcrypt.hash(password, 10);
-    //const hashedPassword2 = await bcrypt.hash(hashedPassword, 10);
-    // console.log(`Hashed password: ${hashedPassword}`)
-    // console.log(`Password provided by user: ${password}`);
-    // console.log(`Password stored in database: ${user.password}`);
-    // console.log(`Result of password comparison: ${validPassword}`);
     if (!validPassword) {
       return res.status(401).send('Invalid password');
     }
